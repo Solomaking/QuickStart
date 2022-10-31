f8d02d01c51a4e050219ceaf4b7221ae84a241ed
Solomaking <obailodv13@gmail.com>
2022-10-31 09:54:23 +0200

    Lesson 11

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.setProperty(
+                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
+        );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
     }
 }
